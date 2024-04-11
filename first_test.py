e16088db2ae6b3fdc50a4f38a1427bd000c59c77
Alex Pren <alexanderpren@gmail.com>
2024-04-10 18:42:05 -0600

    filter works by visible colums

--- first_test.py
+++ first_test.py
@@ -5,9 +5,14 @@ import os
 import logging
 
 
-VALUE_TO_WRITE = "Lamdi"
+CLIENT_NAME = "Lamdi"
 ACCOUNT_NAME_TO_SEARCH = "Cash Checking"
 NUMBER_TO_SEARCH = "1010000"
+COLUMN_ARK_TRANSACTION_FILTER = "column-header-filter-2"
+COLUMN_ATTRIBUTE_FILTER = "column-header-filter-3"
+COLUMN_COLUMN_FINANCIAL_STATEMENT_FILTER = "column-header-filter-7"
+COLUMN_STATUS_FILTER = "column-header-filter-9"
+ARK_TRANSACTION_FILTER_VALUE = "General Expense"
 
 
 class TestWebPageLogin(BaseCase):
@@ -36,21 +41,27 @@ class TestWebPageLogin(BaseCase):
     def test_login(self):
         # Open the web page
         logging.debug("Start Login Test")
-        self.open(os.getenv("BASE_URL"))
-        self.type('input[name="email"]', os.getenv("USERNAME_TEST"))
-        self.type('input[name="password"]', os.getenv("PASSWORD_TEST"))
-        self.click('button[type="submit"]')
-        self.wait(2)
+        try:
+            self.open(os.getenv("BASE_URL"))
+            self.type('input[name="email"]', os.getenv("USERNAME_TEST"))
+            self.type('input[name="password"]', os.getenv("PASSWORD_TEST"))
+            self.click('button[type="submit"]')
+            self.wait(2)
+        except Exception as e:
+            logging.error(f"Error on Login: {e}")
+            self.fail(f"Error: {e}")
 
     def test_search(self):
         self.test_login()
-        
+
         # Click on the "Clients" menu option
+      
         autocomplete_input_xpath = "//input[@id='selector-client']"
         option_with_value_xpath = (
-            f"//ul[@role='listbox']//li[contains(text(), '{VALUE_TO_WRITE}')]"
+            f"//ul[@role='listbox']//li[contains(text(), '{CLIENT_NAME}')]"
         )
-        self.type(autocomplete_input_xpath, VALUE_TO_WRITE)
+        logging.debug("Start Set Value Test on Dropdown")
+        self.type(autocomplete_input_xpath, CLIENT_NAME)
         self.click(option_with_value_xpath)
         self.wait(2)
         self.click("#expand_collapse_sidebar")
@@ -64,22 +75,39 @@ class TestWebPageLogin(BaseCase):
         button = self.find_element("button#general_ledger")
         button.click()
         self.wait_for_element_visible("ul.MuiList-root")
+        self.wait(2)
 
         # Find the specific list item with text "Chart of Accounts (arkGL)"
         chart_of_accounts_item = self.find_element(
             'a[role="menuitem"][href="/fund-nav/chart-of-accounts/aa9c0d49-c899-4116-9729-6d03cda179df"]'
         )
+
         chart_of_accounts_item.click()
         self.wait(2)
 
         # get Input search by account name
-        self.filter_items_by_account_or_number(ACCOUNT_NAME_TO_SEARCH)
-        
+        # self.filter_items_by_account_or_number(ACCOUNT_NAME_TO_SEARCH)
+
         # get Input search by number
-        self.filter_items_by_account_or_number(NUMBER_TO_SEARCH)
+        # self.filter_items_by_account_or_number(NUMBER_TO_SEARCH)
+        # get Input search by selecting columns
+        self.filter_items_by_selecting_columns(
+            COLUMN_ARK_TRANSACTION_FILTER, ARK_TRANSACTION_FILTER_VALUE
+        )
+        self.reset_filters(COLUMN_ARK_TRANSACTION_FILTER)
+       
+           
 
     def filter_items_by_account_or_number(self, filter_value):
-        print("Filtering items by account or number")
+        """
+        Filters items by account or number.
+
+        Args:
+            filter_value (str): The value to filter by.
+
+        Returns:
+            None
+        """
         self.wait_for_element_visible("input#search_accounts")
         self.assert_element_visible("input#search_accounts")
         self.click("input#search_accounts")
@@ -92,7 +120,7 @@ class TestWebPageLogin(BaseCase):
         grid_with_value_xpath = f"//div[@role='grid']"
         grid_table = self.find_element(grid_with_value_xpath)
         self.assert_element_visible(grid_with_value_xpath)
-        
+
         # Clear search items
         self.click("input#search_accounts")
         self.click("input#search_accounts_popover")
@@ -100,6 +128,50 @@ class TestWebPageLogin(BaseCase):
         for i in range(len(filter_value)):
             self.send_keys("input#search_accounts_popover", Keys.BACKSPACE)
         self.wait(2)
+
+    def filter_items_by_selecting_columns(self, column, filter_value):
+        """
+        Filters items by selecting columns.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        # Add your code here to filter items by selecting columns     
+        self.wait_for_element_visible("div[role='grid']")
+        grid_with_value_xpath = f"//div[@role='grid']"
+        grid_table = self.find_element(grid_with_value_xpath)
+        self.wait_for_element_visible(f"div#{column}", timeout=10)
+        self.click(f"div#{column}")
+        self.wait_for_element_visible("div#popover_filter_text")
+        self.uncheck_if_checked("label#check_all span input")
+        self.check_if_unchecked('input[name="' + filter_value + '"]')
+        self.click("#btn_apply")
+        
+    def reset_filters(self, column):
+        """
+        Reset all filters.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        self.wait_for_element_visible("div[role='grid']")
+        grid_with_value_xpath = f"//div[@role='grid']"
+        grid_table = self.find_element(grid_with_value_xpath)
+        self.wait_for_element_visible(f"div#{column}", timeout=10)
+        self.click(f"div#{column}")
+        self.wait_for_element_visible("div#popover_filter_text")
+        self.wait_for_element_visible("button#btn_clear")
+        self.click("button#btn_clear")
+        self.click("#btn_apply")
+        self.wait(2)
+       
+        
 
 
 if __name__ == "__main__":
